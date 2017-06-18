601d5e31351e2b223ac790595abae308b54c12ab
Jeewanjot <jeewanjotSingh09@gmail.com>
2017-06-18 08:32:09 -0400

    CozaLozaWoza

---
+++ CozaLozaWoza/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CozaLozaWoza
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1, end = 110;
+            for (int i = start; i <= end; ++i)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("Coza");
+
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Loza");
+
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write("Woza");
+
+                }
+                else if (i % 3 != 0 || i % 5 != 0 || i % 7 != 0)
+                {
+                    Console.Write(i);
+                }
+                if (i % 11 == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.Write("");
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
